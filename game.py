63346f24ccd978092de7f7457544bc3ca1b209a3
Charlotte Sasson <charlottesasson@MacBook-Pro.home>
2021-12-27 21:42:04 +0100

    creating grid

--- game.py
+++ game.py
@@ -1,9 +1,13 @@
-class Cell:
-    def __init__(self) -> None:
-        pass
+class Case:
+    MAX_ABCISS = 10
+    MAX_ORDINATE = 10
 
-    def die(self):
-        raise NotImplementedError
-    
-    def appear(self):
+    def __init__(self, down_left_corner_absciss, down_left_corner_ordinate) -> None:
+        self.down_left_corner_absciss = down_left_corner_absciss
+        self.down_left_corner_ordinate = down_left_corner_ordinate
+
+    def create_grid(self):
+        for abciss in range(self.MAX_ABCISS):
+            for ordinate in range(self.MAX_ORDINATE):
+                case = Case(abciss, ordinate)
         raise NotImplementedError
